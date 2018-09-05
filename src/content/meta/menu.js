4ccae14afe08caabbf894387ddf09c3daeb4aad3
Brian Winkers <bwinkers@gmail.com>
2018-09-05 11:25:33 -0700

    Update menu

--- src/content/meta/menu.js
+++ src/content/meta/menu.js
@@ -5,12 +5,7 @@ import BlogIcon from 'react-feather/dist/icons/book';
 
 const menu = [
   { label: 'Home', to: '/', icon: HomeIcon },
-  { label: 'Blog', to: '/blog', icon: BlogIcon },
-  { label: 'Experience', to: '/experience', icon: BlogIcon },
-  { label: 'Catalog', to: '/catalog', icon: FolderIcon },
-  { label: 'About', to: '/about', icon: InfoIcon },
-  { label: 'Counter', to: '/counter', icon: InfoIcon },
-  { label: 'Info', to: '/info', icon: InfoIcon }
+  { label: 'Experience', to: '/experience', icon: BlogIcon }
 ];
 
 export default menu;
