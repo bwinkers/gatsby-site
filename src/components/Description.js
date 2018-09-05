e07cd455bb896758a042e721c788fc7ec9a1537e
Brian Winkers <bwinkers@gmail.com>
2018-09-05 11:22:37 -0700

    Description component

---
+++ src/components/Description.js
@@ -0,0 +1,37 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _moment = require("moment");
+
+var _gatsby = require("gatsby");
+
+var _emotion = require("emotion");
+
+var _experience = _interopRequireDefault(require("../styles/description"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Description = function Description(props) {
+  var _props$themeStyle = props.themeStyle,
+      themeStyle = _props$themeStyle === void 0 ? _experience.default : _props$themeStyle,
+      _props$customStyle = props.customStyle,
+      customStyle = _props$customStyle === void 0 ? '' : _props$customStyle;
+  return _react.default.createElement("div", {
+    className: (0, _emotion.cx)(themeStyle, customStyle)
+  },  _react.default.createElement("div", null, props.description));
+};
+
+Description.propTypes = {
+  themeStyle: _propTypes.default.string,
+  customStyle: _propTypes.default.string
+};
+var _default = Description;
+exports.default = _default;
