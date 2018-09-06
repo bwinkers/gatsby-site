b6f78b93e74afeea0868398674ebb5def679d618
Brian Winkers <bwinkers@gmail.com>
2018-09-05 20:06:48 -0700

    Updated styles

--- src/styles/employment-dates.js
+++ src/styles/employment-dates.js
@@ -12,7 +12,7 @@ var _facepaint = _interopRequireDefault(require("facepaint"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  h1 {\n    letter-spacing: -0.03em;\n    margin-bottom: 0.25em;\n    line-height: 1.1;\n\n    ", ";\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  p {\n    text-align: center; font-style: italic; font-size: larger \n  }\n\n "]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -23,12 +23,6 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var breakpoints = [768, 1024];
-var mq = (0, _facepaint.default)(breakpoints.map(function (bp) {
-  return "@media (min-width: ".concat(bp, "px)");
-}));
-var employmentDatesStyle = (0, _emotion.css)(_templateObject(), mq({
-  fontSize: ['2em', '2.4em', '2.8em'] 
-}));
+var employmentDatesStyle = (0, _emotion.css)(_templateObject());
 var _default = employmentDatesStyle;
 exports.default = _default;
--- src/styles/sub-heading.js
+++ src/styles/sub-heading.js
@@ -8,7 +8,7 @@ exports.default = void 0;
 var _emotion = require("emotion");
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  p { font-weight: bold ; font-style: italic } \n"]);
+  var data = _taggedTemplateLiteral(["\n div {\n    display: flex; \n height: 100%; \n }\n\n .company {\n  float: left; \n font-size: 1.6em;  \n align-self: flex-end; \n }\n\n  .location {\n  margin-left: auto; \n font-size: 1.4em;  \n align-self: flex-end; \n }\n\n .clear {\n    clear: left; \n  }\n\n  "]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -19,6 +19,6 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var descriptionStyle = (0, _emotion.css)(_templateObject());
-var _default = descriptionStyle;
+var subHeadingStyle = (0, _emotion.css)(_templateObject());
+var _default = subHeadingStyle;
 exports.default = _default;
